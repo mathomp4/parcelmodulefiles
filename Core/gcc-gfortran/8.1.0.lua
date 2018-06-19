a3a50b721256d466a4ad851874ef5fe4f60b733f
Matthew Thompson <matthew.thompson@nasa.gov>
2018-06-19 10:00:11 -0400

    Commit new files

---
+++ Core/gcc-gfortran/8.1.0.lua
@@ -0,0 +1,35 @@
+-- stub routine for gcc-gfortran 8.1.0 --
+-- built with:
+-- First in the source, if you don't have wget, 
+--     edit ./contrib/download_prerequisites and change wget to curl -O
+-- Next, run contrib/download_prerequisites
+--
+-- Then, in a build directory:
+--
+-- ../gcc-8.1.0/configure --prefix=/Users/mathomp4/installed/Core/gcc-gfortran/8.1.0 --enable-languages=c,c++,fortran --disable-multilib | & tee configure.log
+--
+
+family("Compiler")
+
+local version = "8.1.0"
+local installdir = "/Users/mathomp4/installed/Core/gcc-gfortran"
+local pkgdir = pathJoin(installdir,version)
+local bindir = pathJoin(pkgdir,"bin")
+
+-- Setup Modulepath for packages built by this compiler
+local mroot = os.getenv("MODULEPATH_ROOT")
+local mdir  = pathJoin(mroot,"Compiler/gcc-gfortran-8.1.0")
+prepend_path("MODULEPATH", mdir)
+
+setenv("CC",pathJoin(bindir,"gcc"))
+setenv("CXX",pathJoin(bindir,"g++"))
+setenv("FC",pathJoin(bindir,"gfortran"))
+setenv("F90",pathJoin(bindir,"gfortran"))
+setenv("ESMA_FC","gfortran")
+
+prepend_path("PATH",pathJoin(pkgdir,"bin"))
+prepend_path("LD_LIBRARY_PATH",pathJoin(pkgdir,"lib"))
+prepend_path("DYLD_LIBRARY_PATH",pathJoin(pkgdir,"lib"))
+prepend_path("LIBRARY_PATH",pathJoin(pkgdir,"lib"))
+prepend_path("INCLUDE",pathJoin(pkgdir,"include"))
+prepend_path("MANPATH",pathJoin(pkgdir,"share/man"))
