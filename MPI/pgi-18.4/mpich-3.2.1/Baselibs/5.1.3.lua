88ce9a48e261433dc12835fec0e05427f6ae964b
Matthew Thompson <matthew.thompson@nasa.gov>
2018-08-01 10:18:21 -0400

    Add PGI 18.4 additional modules

---
+++ MPI/pgi-18.4/mpich-3.2.1/Baselibs/5.1.3.lua
@@ -0,0 +1,31 @@
+-- [[
+-- This was built using:
+-- $ make -j4 install ESMF_COMM=mpich3 ESMF_COMPILER=pgi CC=pgcc CXX=pgc++ prefix=/Users/mathomp4/installed/MPI/pgi-18.4/mpich-3.2.1/Baselibs/5.1.3/Darwin
+--
+-- ]]
+
+family("Baselibs")
+--prereq("pgi/18.4", "mpich/3.2.1")
+
+local compilername = "pgi-18.4"
+local mpiname = "mpich-3.2.1"
+
+local version = "5.1.3"
+local pathdir = pathJoin("MPI",compilername,mpiname)
+local homedir = os.getenv("HOME")
+local installdir = pathJoin(homedir,"installed")
+local pkgdir = pathJoin(installdir,pathdir,"Baselibs",version)
+
+-- Setup Modulepath for packages built by this MPI stack
+local mroot = os.getenv("MODULEPATH_ROOT")
+local mdir = pathJoin(mroot,"Baselibs",compilername,mpiname)
+prepend_path("MODULEPATH", mdir)
+
+setenv("BASEDIR",pkgdir)
+setenv("basedir",pkgdir)
+setenv("BASEBIN",pathJoin(pkgdir,"Darwin/bin"))
+
+prepend_path("PATH",pathJoin(pkgdir,"Darwin/bin"))
+prepend_path("LD_LIBRARY_PATH",pathJoin(pkgdir,"Darwin/lib"))
+prepend_path("MANPATH",pathJoin(pkgdir,"Darwin/share/man"))
+
