2f847c5b25976c6f40435e18fbf0360936c6ebc6
Matthew Thompson <matthew.thompson@nasa.gov>
2019-03-11 12:12:11 -0400

    Add spackenv

---
+++ Core/SpackEnv.lua
@@ -0,0 +1,18 @@
+-- quick load --
+
+load("Anaconda2/5.3.1")
+load("gcc-gfortran/8.2.0")
+load("openmpi/3.1.2-dstewji")
+
+load("hdf5/1.10.4-qegh3x6")
+load("netcdf/4.6.2-2hsau6w")
+load("netcdf-fortran/4.4.4-v2f6nqe")
+load("veclibfort/0.4.2-uqhoqna")
+load("esmf/8.0.0b14-veclibfort-w345am7")
+
+load("gftl/1.0.0-25mg5xw")
+load("pfunit/3.3.2-mpi-3ygc4rt")
+load("flap/2017-12-18-eq3zxld")
+
+-- load("git/2.20.1-x7hf7xr")
+-- load("cmake/3.13.2-ff6bfsc")
