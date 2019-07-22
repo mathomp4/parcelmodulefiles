e36e8bb2102d4ee9890a447bd81c61ae0c93966c
Matthew Thompson <matthew.thompson@nasa.gov>
2019-07-22 07:35:22 -0400

    Updates. Move to gcc 9.1

--- Core/DevEnv.lua
+++ Core/DevEnv.lua
@@ -2,6 +2,6 @@
 
 load("GMAOpyD/Ana2019.03_py2.7")
 load("MKL/2019.3.199")
-load("gcc-gfortran/8.3.0")
-load("openmpi/4.0.0")
-load("Baselibs/5.2.4")
+load("gcc-gfortran/9.1.0")
+load("openmpi/4.0.1")
+load("Baselibs/5.2.6")
