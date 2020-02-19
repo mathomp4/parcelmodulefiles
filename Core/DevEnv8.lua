8b1d1613a6c81c887f18b8a433a9b29690fa23d6
Matthew Thompson <matthew.thompson@nasa.gov>
2020-02-19 09:13:47 -0500

    DevEnv is now StdEnv

--- Core/DevEnv8.lua
+++
@@ -1,7 +0,0 @@
--- quick load --
-
-load("GMAOpyD/Ana2019.03_py2.7")
-load("MKL/2019.3.199")
-load("gcc-gfortran/8.3.0")
-load("openmpi/4.0.0")
-load("Baselibs/5.2.4")
--- Core/StdEnv.lua
+++ Core/StdEnv.lua
@@ -3,5 +3,5 @@
 load("GMAOpyD/Ana2019.03_py2.7")
 load("MKL/2019.3.199")
 load("gcc-gfortran/9.2.0")
-load("openmpi/4.0.1-MPI1")
-load("Baselibs/5.1.7")
+load("openmpi/4.0.1")
+load("Baselibs/6.0.4")
