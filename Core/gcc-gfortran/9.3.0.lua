9d3501ff0e98b921fe1ee5dc15f5661ec1ab488d
Matthew Thompson <matthew.thompson@nasa.gov>
2020-04-21 10:05:23 -0400

    Updates. gcc 9.3, etc.

---
+++ Core/gcc-gfortran/9.3.0.lua
@@ -0,0 +1,44 @@
+-- stub routine for gcc-gfortran 9.3.0 --
+-- built with:
+--
+-- First in the source, if you don't have wget, 
+--     edit ./contrib/download_prerequisites and change wget to curl -O
+-- Next, run contrib/download_prerequisites
+--
+-- NOTE: Decided to follow what homebrew does and add the --with-sysroot option. This seems needed on
+--       macOS now
+--
+-- Then, in a build directory:
+--
+-- ../gcc-9.3.0/configure --prefix=$HOME/installed/Core/gcc-gfortran/9.3.0 --enable-languages=c,c++,fortran --with-sysroot=/Library/Developer/CommandLineTools/SDKs/MacOSX.sdk |& tee configure.log
+--
+-- make -j4 |& tee make.log
+-- make install |& tee makeinstall.log
+-- make check |& tee makecheck.log
+--
+
+family("Compiler")
+
+local version = "9.3.0"
+local homedir = os.getenv("HOME")
+local installdir = pathJoin(homedir,"installed/Core/gcc-gfortran")
+local pkgdir = pathJoin(installdir,version)
+local bindir = pathJoin(pkgdir,"bin")
+
+-- Setup Modulepath for packages built by this compiler
+local mroot = os.getenv("MODULEPATH_ROOT")
+local mdir  = pathJoin(mroot,"Compiler/gcc-gfortran-9.3.0")
+prepend_path("MODULEPATH", mdir)
+
+setenv("CC",pathJoin(bindir,"gcc"))
+setenv("CXX",pathJoin(bindir,"g++"))
+setenv("FC",pathJoin(bindir,"gfortran"))
+setenv("F90",pathJoin(bindir,"gfortran"))
+setenv("ESMA_FC","gfortran")
+
+prepend_path("PATH",pathJoin(pkgdir,"bin"))
+prepend_path("LD_LIBRARY_PATH",pathJoin(pkgdir,"lib"))
+prepend_path("DYLD_LIBRARY_PATH",pathJoin(pkgdir,"lib"))
+prepend_path("LIBRARY_PATH",pathJoin(pkgdir,"lib"))
+prepend_path("INCLUDE",pathJoin(pkgdir,"include"))
+prepend_path("MANPATH",pathJoin(pkgdir,"share/man"))
--- MPI/gcc-gfortran-9.3.0/openmpi-4.0.3/Baselibs/6.0.11.lua
+++ MPI/gcc-gfortran-9.3.0/openmpi-4.0.3/Baselibs/6.0.11.lua
@@ -1,17 +1,17 @@
 -- [[
 --
 -- This was built using:
--- $ make -j4 install ESMF_COMM=openmpi ESMF_COMPILER=gfortran prefix=$HOME/installed/MPI/gcc-gfortran-9.2.0/openmpi-4.0.3/Baselibs/6.0.7/Darwin |& tee makeinstall.gcc-gfortran-9.2.0_openmpi-4.0.3.log
+-- $ make -j4 install ESMF_COMM=openmpi ESMF_COMPILER=gfortran prefix=$HOME/installed/MPI/gcc-gfortran-9.3.0/openmpi-4.0.3/Baselibs/6.0.11/Darwin |& tee makeinstall.gcc-gfortran-9.3.0_openmpi-4.0.3.log
 --
 -- ]]
 
 family("Baselibs")
---prereq("gcc-gfortran/9.2.0", "openmpi/4.0.3")
+--prereq("gcc-gfortran/9.3.0", "openmpi/4.0.3")
 
-local compilername = "gcc-gfortran-9.2.0"
+local compilername = "gcc-gfortran-9.3.0"
 local mpiname = "openmpi-4.0.3"
 
-local version = "6.0.7"
+local version = "6.0.11"
 local pathdir = pathJoin("MPI",compilername,mpiname)
 local homedir = os.getenv("HOME")
 local installdir = pathJoin(homedir,"installed")
