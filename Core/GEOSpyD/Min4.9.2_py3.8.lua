c76a14ea96fdfe3b39bd66c8916dc59ff8ca019a
Matthew Thompson <matthew.thompson@nasa.gov>
2020-12-01 12:20:00 -0500

    Update GEOSpyD

--- Core/GEOSpyD/Min4.9.2_py3.8.lua
+++ Core/GEOSpyD/Min4.9.2_py3.8.lua
@@ -2,7 +2,11 @@
 --
 -- Installed using the Anaconda installer script on github:
 --
---   git@github.com:mathomp4/anaconda.git
+--   git@github.com:GMAO-SI-Team/anaconda.git
+--
+-- to install:
+--
+--   ./install_miniconda.bash --python_version 3.8 --miniconda_version 4.9.2 --prefix /Users/mathomp4/GEOSpyD |& tee install_mini3.log
 --
 
 family("Python")
@@ -10,10 +14,10 @@ family("Python")
 local homedir = os.getenv("HOME")
 local installdir = pathJoin(homedir,"GEOSpyD")
 
-local anaconda_version = "2019.10"
-local python_version = "2.7"
-local geospyd_version = anaconda_version .. "_py" .. python_version
-local builddate = "2020-02-13"
+local miniconda_version = "4.9.2"
+local python_version = "3.8"
+local geospyd_version = miniconda_version .. "_py" .. python_version
+local builddate = "2020-12-01"
 
 local pathdir = pathJoin(geospyd_version,builddate)
 local pkgdir = pathJoin(installdir,pathdir)
@@ -27,3 +31,4 @@ unsetenv("ESMA_F2PY")
 
 prepend_path("PATH",pathJoin(pkgdir,"bin"))
 prepend_path("PROJ_LIB",pathJoin(pkgdir,"share/proj"))
+prepend_path("MANPATH",pathJoin(pkgdir,"share/man"))
