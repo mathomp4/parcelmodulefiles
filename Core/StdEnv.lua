4db3d71b3d655f5c4f350f0eab1174ffb810f396
Matthew Thompson <matthew.thompson@nasa.gov>
2020-12-01 12:24:33 -0500

    Update StdEnv

--- Core/StdEnv.lua
+++ Core/StdEnv.lua
@@ -1,7 +1,7 @@
 -- quick load --
 
 load("GEOSpyD/Min4.8.3_py2.7")
-load("MKL/2020.0.166")
-load("gcc-gfortran/10.2.0")
-load("openmpi/4.0.4")
-load("Baselibs/6.0.16")
+load("MKL/2020.4.301")
+load("gcc-gfortran")
+load("openmpi")
+load("Baselibs")
