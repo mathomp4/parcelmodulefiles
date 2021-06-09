3d6daace2f7ed2c259963d8f0836222a65b36c7e
Matthew Thompson <matthew.thompson@nasa.gov>
2021-06-09 08:22:59 -0400

    Clean up gcc 10.3

--- MPI/gcc-gfortran-10.3.0/openmpi-4.1.0/Baselibs/6.2.5.lua
+++ MPI/gcc-gfortran-10.3.0/openmpi-4.1.0/Baselibs/6.2.5.lua
@@ -1,7 +1,7 @@
 -- [[
 --
 -- This was built using:
--- $ make -j6 install ESMF_COMM=openmpi ESMF_COMPILER=gfortran prefix=$HOME/installed/MPI/gcc-gfortran-10.3.0/openmpi-4.1.0/Baselibs/6.2.0-TEST/Darwin |& tee makeinstall.gcc-gfortran-10.3.0_openmpi-4.1.0.log
+-- $ make -j6 install ESMF_COMM=openmpi ESMF_COMPILER=gfortran prefix=$HOME/installed/MPI/gcc-gfortran-10.3.0/openmpi-4.1.0/Baselibs/6.2.5/Darwin |& tee makeinstall.gcc-gfortran-10.3.0_openmpi-4.1.0.log
 --
 -- NOTE: To build curl on Parcel, I had to do:
 --
