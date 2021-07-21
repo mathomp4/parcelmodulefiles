22b6d5196e7587431f74a78cca57a25577fc2632
Matthew Thompson <matthew.thompson@nasa.gov>
2021-07-21 10:16:00 -0400

    update MKL

--- Core/MKL/2021.3.0.lua
+++ Core/MKL/2021.3.0.lua
@@ -3,20 +3,21 @@
 
 family("Math")
 
-local version = "2018.1.126"
+local version = "2021.3.0"
 -- local pathdir = pathJoin("Core","Anaconda2",version)
 -- local homedir = os.getenv("HOME")
 -- local installdir = pathJoin(homedir,"installed")
-local pkgdir = pathJoin("/opt/intel/compilers_and_libraries_2018.1.126/mac/mkl")
+local pkgdir = pathJoin("/opt/intel/oneapi/mkl",version)
 
 -- Setup Modulepath for packages built by this compiler
 local mroot = os.getenv("MODULEPATH_ROOT")
-local mdir  = pathJoin(mroot,"Math/MKL-2018.1.126")
+local mdir  = pathJoin(mroot,"Math/MKL-2021.3.0")
 prepend_path("MODULEPATH", mdir)
 
 setenv('MKLROOT',pkgdir)
+setenv('MKLPATH',pathJoin(pkgdir,"lib"))
 
 prepend_path("PATH",pathJoin(pkgdir,"bin"))
-prepend_path("MANPATH",pathJoin(pkgdir,"man"))
+-- prepend_path("MANPATH",pathJoin(pkgdir,"man"))
 prepend_path("LD_LIBRARY_PATH",pathJoin(pkgdir,"lib"))
 prepend_path("DYLD_LIBRARY_PATH",pathJoin(pkgdir,"lib"))
