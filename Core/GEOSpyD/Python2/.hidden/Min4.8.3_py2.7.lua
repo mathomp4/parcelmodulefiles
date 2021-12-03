37ab058e6b92f586d9506ee0b946904199f3bf7e
Matthew Thompson <matthew.thompson@nasa.gov>
2021-12-03 08:09:38 -0500

    Can't install Python2 anymore

--- Core/GEOSpyD/Python2/.hidden/Min4.8.3_py2.7.lua
+++ Core/GEOSpyD/Python2/.hidden/Min4.8.3_py2.7.lua
@@ -8,6 +8,16 @@
 --
 --   ./install_miniconda.bash --python_version 2.7 --miniconda_version 4.8.3 --prefix /Users/mathomp4/GEOSpyD --conda
 --
+-- This then failed because of:
+--
+--   https://github.com/conda/conda/issues/10361
+--
+-- So I hand-edited the lib/python2.7/ctypes/util.py a la:
+--
+--   https://stackoverflow.com/a/65175354/1876449
+--
+-- and commented out the top of the Miniconda installer script
+--
 -- NOTE: You'll need to set the date below!
 --
 
