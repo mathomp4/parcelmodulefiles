c365174330c730ee4c4df58fdda0c31139197bb3
Matthew Thompson <matthew.thompson@nasa.gov>
2023-02-10 09:07:20 -0500

    Add MKL modules

---
+++ Core/MKL/2022.2.0.lua
@@ -0,0 +1,20 @@
+-- File for MKL
+--
+
+family("Math")
+
+local version = "2022.2.0"
+local pkgdir = pathJoin("/opt/intel/oneapi/mkl",version)
+
+-- Setup Modulepath for packages built by this compiler
+local mroot = os.getenv("MODULEPATH_ROOT")
+local mdir  = pathJoin(mroot,"Math/MKL-2022.2.0")
+prepend_path("MODULEPATH", mdir)
+
+setenv('MKLROOT',pkgdir)
+setenv('MKLPATH',pathJoin(pkgdir,"lib"))
+
+prepend_path("PATH",pathJoin(pkgdir,"bin"))
+-- prepend_path("MANPATH",pathJoin(pkgdir,"man"))
+prepend_path("LD_LIBRARY_PATH",pathJoin(pkgdir,"lib"))
+prepend_path("DYLD_LIBRARY_PATH",pathJoin(pkgdir,"lib"))
